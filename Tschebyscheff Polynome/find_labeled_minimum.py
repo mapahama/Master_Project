d596f7cb2e70ac21777a798200b18912edc40318
kirilov <kirilov@uni-hildesheim.de>
2025-07-08 15:19:56 +0200

    CKKS Context verbessern -  Multiplikative Tiefe - 10

--- Tschebyscheff Polynome/find_labeled_minimum.py
+++ Tschebyscheff Polynome/find_labeled_minimum.py
@@ -39,7 +39,7 @@ import time
 ################ Start Polynom ####################
 
 # 1. Tschebyscheff-Approximation der Signumfunktion
-CHEBYSHEV_DEGREE = 9 # Grad des Tschebyscheff-Polynoms. Ein höherer Grad -> genauere Approximation, erhöht aber auch Multiplikationstiefe, Rauschen und Berechnungszeit in CKKS
+CHEBYSHEV_DEGREE = 5 # Grad des Tschebyscheff-Polynoms. Ein höherer Grad -> genauere Approximation, erhöht aber auch Multiplikationstiefe, Rauschen und Berechnungszeit in CKKS
 x_vals = np.linspace(-1, 1, 2000) # Erstellt ein Array von 2000 gleichmäßig verteilten Zahlen im Intervall von -1 bis +1
 
 sign_vals = np.sign(x_vals) #  Berechnet Signumfunktion (Vorzeichenfunktion) für jeden Wert in x_vals
@@ -91,18 +91,18 @@ plt.grid(True); plt.legend(); plt.show()
 
 context = ts.context(
     ts.SCHEME_TYPE.CKKS,
-    poly_modulus_degree=32768, # 16384 ->error  # Grad des Polynommoduls. Ein höherer Grad erhöht die Sicherheit und die Kapazität des Ciphertextes, verlangsamt aber die Berechnung.
+    poly_modulus_degree=16384 ,# 32768, # 8192 ->error  # Grad des Polynommoduls. Ein höherer Grad erhöht die Sicherheit und die Kapazität des Ciphertextes, verlangsamt aber die Berechnung.
         # Kette von Primzahlen-Moduln - Multiplikationstiefe. Wen mehrere Moduln, dann langsamere Berechnung. 
         # Erlaubt aber mehrere Multiplikationen. 
         # Ziel: möglichst kleine aber ausreichende für die Multiplikationen Tiefe, sonst Error: scale out of bounds
-    coeff_mod_bit_sizes=[60] + [40]*12 + [60] # Kette von Primzahlen-Moduln. 
+    coeff_mod_bit_sizes=[50] + [25]*10 + [50] #  Kette von Primzahlen-Moduln. 
 )
 
 # Galois Keys. Generiert spezielle Schlüssel (Galois-Schlüssel), die für Rotationen in Vektoren benötigt werden.
 # Die Funktion .polyval() nutzt diese intern, um die Potenzen von x (x^2, x^3, ...) effizient zu berechnen.
 context.generate_galois_keys()
 # Setzt den globalen Skalierungsfaktor. Dies ist ein entscheidender Parameter bei CKKS, der die Genauigkeit der Fließkommazahlen bestimmt. 2^40 ist ein üblicher hoher Wert für gute Präzision.
-context.global_scale = 2**40
+context.global_scale = 2**25
 #context.auto_rescale = True # nicht nötig !
 
 # liste im Bereich [0, 1] normieren, damit Differenz von 2 Zahlen im Bereich [-1, 1] liegt
@@ -128,9 +128,9 @@ def homomorphic_sgn(a_minus_b):
                                # grad 9    (ist Min Wert: -0.03  --  soll Min Wert: 0.0)  
     # coeffs = [0, 3.4375, 0, -4.6875, 0, 2.8125, 0, -0.5625] # grad 7    (ist Min Wert:  0.29  --  soll Min Wert: 0.36)
                                                               # grad 7    (ist Min Wert: -0.06  --  soll Min Wert: 0.0)
-    # coeffs = [0, 1.5708, 0, -0.6459, 0,  0.0796, 0, -0.0046] # grad 7   (ist Min Wert: 0.37 -- soll Min Wert: 0.36)  <-
+    coeffs = [0, 1.5708, 0, -0.6459, 0,  0.0796, 0, -0.0046] # grad 7   (ist Min Wert: 0.37 -- soll Min Wert: 0.36)  <-
                                                                # grad 7   (ist Min Wert: 0.01  -- soll Min Wert: 0.0)  <-
-    coeffs = [0, 1.5708,  0, -0.6704, 0, 0.1346, 0, -0.0125, 0, 0.0005] # grad 9   (ist Min Wert: 0.367  -- soll Min Wert: 0.36) <-
+    # coeffs = [0, 1.5708,  0, -0.6704, 0, 0.1346, 0, -0.0125, 0, 0.0005] # grad 9   (ist Min Wert: 0.367  -- soll Min Wert: 0.36) <-
                                                                           # grad 9   (ist Min Wert: 0.007   -- soll Min Wert: 0.0) <-
     # coeffs = [0, 4.375, 0, -8.203125, 0, 8.4375, 0, -3.9375, 0, 0.6875] # grad 9   (ist Min Wert: 0.22  -- soll Min Wert: 0.36)
                                                                           # grad 9   (ist Min Wert: -0.13   -- soll Min Wert: 0.0)
@@ -235,15 +235,19 @@ plain_values = [6.1, 6.2, 6.3, 6.01]
 labels_plain = ["krank", "gesund", "krank", "gesund"]
 label_map = {"krank": 1.0, "gesund": 0.0}
 print(f"Originale Liste: {plain_values}")
+print(f"Zugehörige Labels: {labels_plain}")
 
 
 norm_plain_values = min_max_scale(plain_values)
+print(f"Normierte Liste: {norm_plain_values}")
 print(f"Erwartetes Minimum: {min(norm_plain_values)}")
 
 norm_labels = [label_map[lbl] for lbl in labels_plain]
+print(f"Normierte Labels: {norm_labels}")
 encrypted_values = [ts.ckks_vector(context, [v]) for v in norm_plain_values]
 encrypted_labels = [ts.ckks_vector(context, [l]) for l in norm_labels]
 enc_label_list = list(zip(encrypted_values, encrypted_labels))
+print(f"enc_label_list: {enc_label_list}")
 
 start_time = time.time()
 result_encrypted, result_label = find_min_tournament_with_labels(enc_label_list)
